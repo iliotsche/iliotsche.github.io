7e717f116f457617d62bf63d21af9e92a8b0215f
iliotsche <ilias.lakniz@studentfr.ch>
2026-07-01 16:50:14 +0200

    Python

---
+++ python.py
@@ -0,0 +1,27 @@
+print("Wilkommen bei meinem zahlen rate spiel!")
+
+import random
+
+Schwiereigkeit = input("Willst du es einfach, mittel oder schwer?")
+
+if Schwiereigkeit == "Einfach" or Schwiereigkeit == "einfach":
+    print("Du hast dich fuer einfach entschieden!")
+    Zahl_Computer = random.randint(1, 10)
+
+elif Schwiereigkeit == "Mittel" or Schwiereigkeit == "mittel":
+    print("Du hast dich fuer Mitte entscheiden!")    
+    Zahl_Computer = random.randint(1, 50)
+
+elif Schwiereigkeit == "Schwer" or Schwiereigkeit == "schwer":
+    print("Du hast dich fuer schwer entscheiden!")
+    Zahl_Computer = random.randint(1, 100)
+
+else:
+    print("Error: Gib bitte Einfach, Mittel oder Schwer ein!")
+
+
+Zahl_Spieler = int(input("Bitte waehl deine Zahl"))
+
+while True:
+    if Zahl_Spieler == Zahl_Computer:
+        Print(f"")
